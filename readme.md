191e8d0abc292e9296cd6deeca9d5b619a08512f
Tomas Thelander <tomas.thelander0@gmail.com>
2012-09-08 18:46:45 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,39 @@
+tablejs
+=======
+
+tablejs is a small jQuery plug-in to automatically create HTML-tables from CSV-files.
+
+How to use
+----------
+
+Include both jQuery and table.js.
+
+Create a table-element with a source specified: <table src="values.csv"></table>
+
+Call tablejs:
+
+    $('table').tablejs({
+        // options goes here
+    });
+
+
+Options
+-------
+
+    ignorecolumns (array) : An array of integers indicating which (if any) columns should be ignored (starts at 0)
+    ignorerows (array) : An array of integers indicating which (if any) rows should be ignored (starts at 0)
+    separator (char (default: ',')) : The character used for separating values in the CSV-file
+    titlesfirst (bool (default: false)) : If the first row should use <th>-tags instead of <td>
+    wrap (char (default: '"')) : The characters used when values in the file are wrapped
+
+
+Styling
+-------
+
+All generated elements includes specific classes to be used when styling.
+
+    class=tablejs-table : The main table
+    class=tablejs-tr : A row (<tr>)
+    class=tablejs-trh : The top row (the first <tr>)
+    class=tablejs-th : Header element (<th>)
+    class=tablejs-td : Element (<td>)
---
+++ table.js
@@ -0,0 +1,123 @@
+/**
+ *
+ *
+ *
+ *
+ */
+
+(function($) {
+    var parseRow = function(row, sep, wrap, ignore) {
+        elems = row.split(sep);
+        
+        for (i = 0; i < elems.length; i++)
+        {
+            if (elems[i][0] == wrap && elems[i][elems[i].length-1] == wrap)
+            {
+                elems[i] = elems[i].substr(1, elems[i].length-2);
+            }
+            else if (elems[i][0] == wrap && elems[i][elems[i].length-1] != wrap)
+            {
+                for (j = i+1; j < elems.length; j++)
+                {
+                    elems[i] += ',' + elems[j];
+                    
+                    if (elems[j][elems[j].length-1] == wrap)
+                    {
+                        break;
+                    }
+                }
+                
+                elems.splice(i+1, j-i);
+                
+                elems[i] = elems[i].substr(1, elems[i].length-2);
+            }
+            
+        }
+        
+        for (i = 0; i < elems.length; i++)
+        {
+            if ($.inArray(i, ignore) > -1)
+            {
+                delete elems[i];
+            }
+        }
+        
+        return elems;
+    };
+    
+    $.fn.tablejs = function(attr) {
+        if (attr == null) attr = {};
+        
+        attr = $.extend({
+            'titlesfirst' : false,
+            'ignorerows' : [],
+            'separator' : ',',
+            'wrap' : '"',
+            'ignorecolumns' : []
+        }, attr);
+        
+        
+        if (this.is('table') && this.attr('src') != null)
+        {
+            var csv = this.attr('src');
+            var elm = this;
+            var text = '';
+            
+            $.get(csv, function(data){
+                elm.removeAttr('src');
+                elm.addClass('tablejs-table');
+                
+                data = data.replace(/(\r\n)/gm,"\n");
+                datarows = data.split("\n");
+                
+                if ($.inArray(0,attr.ignorerows) == -1)
+                {
+                    if (attr.titlesfirst)
+                    {
+                        var ts = '<th class="tablejs-th">';
+                        var te = '</th>';
+                    }
+                    else
+                    {
+                        var ts = '<td class="tablejs-td">';
+                        var te = '</td>';
+                    }
+                    
+                    elems = parseRow(datarows[0], attr.separator, attr.wrap, attr.ignorecolumns);
+                    
+                    text += '<tr class="tablejs-tr tablejs-trh">';
+                    
+                    for (i in elems)
+                    {
+                        text += ts + elems[i] + te;
+                    }
+                    
+                    text += '</tr>';
+                }
+                
+                for (r = 1; r < datarows.length; r++)
+                {
+                    if ($.inArray(r,attr.ignorerows) > -1)
+                    {
+                        continue;
+                    }
+                    if (datarows[r] != '')
+                    {
+                        text += '<tr class="tablejs-tr">';
+                        
+                        elems = parseRow(datarows[r], attr.separator, attr.wrap, attr.ignorecolumns);
+                        
+                        for (e in elems)
+                        {
+                            text += '<td class="tablejs-td">' + elems[e] + '</td>';
+                        }
+                        
+                        text += '</tr>';
+                    }
+                }
+                
+                elm.append(text);
+            });
+        }
+    }
+})(jQuery);
