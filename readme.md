638cd03dbd8a12dcc3ecb51b25627c2662d71688
Tomas Thelander <tomas.thelander0@gmail.com>
2012-09-10 19:39:13 +0200

    major update

--- readme.md
+++ readme.md
@@ -3,10 +3,12 @@ tablejs
 
 tablejs is a small jQuery plug-in to automatically create sortable HTML-tables from CSV-files.
 
+Tested in newer versions of Chrome, Firefox, Internet Explorer and Opera.
+
 Live example
 ------------
 
-http://dev.tthe.se/tablejs/
+![dev.tthe.se](http://dev.tthe.se/tablejs/)
 
 How to use
 ----------
@@ -27,12 +29,60 @@ Call tablejs:
 Options
 -------
 
-    ignorecolumns (array) : An array of integers indicating which (if any) columns should be ignored (starts at 0)
-    ignorerows (array) : An array of integers indicating which (if any) rows should be ignored (starts at 0)
-    separator (char (default: ',')) : The character used for separating values in the CSV-file
-    titlesfirst (bool (default: false)) : If the first row contains column titles. Uses <th> instead of <td> and offers sortability.
-    wrap (char (default: '"')) : The characters used when values in the file are wrapped
+Note about counting columns and rows. Counting always starts at 0, so the first column/row has index 0, the second 1 and so on.
 
+When an option below is marked as "CSV column/row" you should pass an index as it appears in the CSV file.
+For example, if you want to hide the third row in the CSV, you add the integer 2 to ignorerows.
+
+When it says "Displayed columns" you should pass an index to a column as it appears in the finished table.
+So if you want the third column in the CSV file to be sorted as dates, and have ignored the first column using ignorecolumns,
+the third column in the file will appear as the second column in the table, and the index you should add to datecolumns is 1 (the second index).
+
+*datecolumns*
+
+    (array) : An array of integers indicating which (if any) columns should be treated as dates and/or times when sorting the table. Displayed columns.
+
+*ignorecolumns*
+
+    (array) : An array of integers indicating which (if any) columns should be ignored (starts at 0). CSV columns.
+
+*ignorerows*
+
+    (array) : An array of integers indicating which (if any) rows should be ignored (starts at 0). CSV rows.
+
+*separator*
+
+    (char (default: ',')) : The character used for separating values in the CSV-file.
+
+*sorting*
+
+    (bool (default: true)) : If the table should be sortable.
+
+*startsorted*
+    (int) : Which (if any) column the table should be sorted after upon creation of the table. Displayed columns.
+
+*symbols*
+
+    (bool (default: true)) : If ascending/descending symbols should be displayed when table is sorted.
+
+*titlesfirst*
+
+    (bool (default: false)) : If the first row contains column titles. Uses <th> instead of <td> and offers sortability.
+
+*wrap*
+
+    (char (default: '"')) : The characters used when values in the file are wrapped.
+
+
+Example Configuration
+---------------------
+
+    $('table').tablejs({
+        'ignorecolumns' : [0], // Ignores the first column in the CSV
+        'ignorerows' : [3], // Ignores the fourth row in the CSV
+        'datecolumns' : [1,3], // The 2nd and 4th displayed columns (3rd and 5th since we have ignored one column) are dates/times
+        'separator' : ';' // Semicolons are used as separators in the CSV, instead of the standard comma
+    });
 
 Styling
 -------
@@ -41,9 +91,27 @@ All generated elements includes specific classes to be used when styling.
 
     class=tablejs-table : The main table
     class=tablejs-tr : A row (<tr>)
+    class=tablejs-tr-rX : Row (<tr>) number X (replace X with a number). This row changes place when sorting.
     class=tablejs-trh : The header row (the first <tr>, if titlesfirst is true)
     class=tablejs-th : Header element (<th>)
     class=tablejs-td : Element (<td>)
+    class=tablejs-td-cX : Elements (<td>) in column number X
+    class=tablejs-td-sort : Elements (<td>) in the column the table is currently sorted after
+    class=tablejs-sort : Links (<a>) in the header used for sorting
+    class=tablejs-sortsymbol : Labels (<span>) with ascending/descending-symbols
+
+Updates
+-------
+
+2012-09-10
+
+    Now possible to sort columns by date
+    Symbols when sorting (ascending/descending) (may be disabled)
+    Turn off sorting
+    Display table sorted by a specified column directly
+    New element classes
+    New design example in index.html
+
 
 Future
 ------
--- table.js
+++ table.js
@@ -9,6 +9,20 @@
  */
 
 (function($) {
+    
+    /**
+     *
+     * parseRow()
+     * Gets the separate values from a CSV-row
+     * 
+     * @param string row , one row of the csv
+     * @param char sep , the separator between values
+     * @param char wrap , the character used when wrapping values
+     * @param array ignore , an array of which columns to ignore
+     *
+     * @return array , an array with the parsed values
+     * 
+     */
     var parseRow = function(row, sep, wrap, ignore) {
         elems = row.split(sep);
         
@@ -48,15 +62,40 @@
         return elems;
     };
     
-    var findInRow = function(row, ci) {
+    /**
+     * 
+     * findInRow()
+     * Returns a specific value som a table row. Used when sorting.
+     *
+     * @param DOM row , a DOM-object of a <tr>-element
+     * @param int ci , the column from which the value is to be fetched
+     * @param array dates , which columns whose values should be returned as timestamps
+     * 
+     */
+    var findInRow = function(row, ci, dates) {
         columns = $(row).find('td');
         column = columns[ci];
         
         text = $(column).text();
-        
         if (isNaN(text))
         {
-            return text;
+            if ($.inArray(parseInt(ci,10), dates) > -1)
+            {
+                d = new Date(text);
+            
+                if (isNaN(d.getTime()))
+                {
+                    return text;
+                }
+                else
+                {
+                    return d.getTime();
+                }
+            }
+            else
+            {
+                return text;
+            }
         }
         else
         {
@@ -64,6 +103,11 @@
         }
     };
     
+    /**
+     *
+     * jQuery.tablejs
+     *
+     */
     $.fn.tablejs = function(attr) {
         if (attr == null) attr = {};
         
@@ -72,10 +116,14 @@
             'ignorerows' : [],
             'separator' : ',',
             'wrap' : '"',
-            'ignorecolumns' : []
+            'ignorecolumns' : [],
+            'datecolumns' : [],
+            'symbols' : true,
+            'startsorted' : false,
+            'sorting' : true
         }, attr);
         
-        
+        // The element must be a table and have a source
         if (this.is('table') && this.attr('src') != null)
         {
             var csv = this.attr('src');
@@ -84,6 +132,7 @@
             
             elm.html('<tr><td>Loading data</td></tr>');
             
+            // Gets CSV data
             $.get(csv, function(data){
                 elm.removeAttr('src');
                 elm.addClass('tablejs-table');
@@ -106,7 +155,24 @@
                                 c--;
                                 continue;
                             }
-                            text += '<th class="tablejs-th"><a href="#" rel="'+ c +'" class="tablejs-sort">' + elems[i] + '</a></th>';
+                            
+                            text += '<th class="tablejs-th">';
+                            
+                            if (attr.sorting)
+                            {
+                                text += '<a href="#" rel="'+ c +'" class="tablejs-sort">' + elems[i] + '</a>';
+                                
+                                if (attr.symbols)
+                                {
+                                    text += ' <span class="tablejs-sortsymbol">&#8211;</span>';
+                                }
+                            }
+                            else
+                            {
+                                text += elems[i];
+                            }
+                            
+                            text += '</th>';
                         }
                     }
                     else
@@ -115,7 +181,7 @@
                         
                         for (i in elems)
                         {
-                            text += '<td class="tablejs-td">' + elems[i] + '</td>';
+                            text += '<td class="tablejs-td tablejs-td-c0">' + elems[i] + '</td>';
                         }
                     }
                     
@@ -130,13 +196,13 @@
                     }
                     if (datarows[r] != '')
                     {
-                        text += '<tr class="tablejs-tr">';
+                        text += '<tr class="tablejs-tr tablejs-tr-r'+ r +'">';
                         
                         elems = parseRow(datarows[r], attr.separator, attr.wrap, attr.ignorecolumns);
                         
                         for (e in elems)
                         {
-                            text += '<td class="tablejs-td">' + elems[e] + '</td>';
+                            text += '<td class="tablejs-td tablejs-td-c'+ e +'">' + elems[e] + '</td>';
                         }
                         
                         text += '</tr>';
@@ -145,6 +211,10 @@
                 
                 elm.html(text);
                 
+                /*
+                    Sorting using a Selection Sort algorithm.
+                    It's quite slow on bigger tables, but then again, tablejs is not built for displaying hundreds or thousands of rows.
+                */
                 elm.find('.tablejs-sort').click(function(e){
                     e.preventDefault();
                     var sortby = $(this).attr('rel');
@@ -152,7 +222,19 @@
                     var mode = ($(this).data('mode') == 'asc' ? 'desc' : 'asc');
                     
                     elm.find('.tablejs-sort').data('mode','');
+                    elm.find('.tablejs-td').removeClass('tablejs-td-sort');
+                    elm.find('.tablejs-td-c' + sortby).addClass('tablejs-td-sort');
                     $(this).data('mode', mode);
+                    
+                    if (attr.symbols)
+                    {
+                        cols = $('tr.tablejs-trh').find('th');
+                        $('span.tablejs-sortsymbol').replaceWith('<span class="tablejs-sortsymbol">&#8211;</span>');
+                        
+                        symbol = (mode == 'asc') ? '&#708;' : '&#709;';
+                        
+                        $(cols[sortby]).find('span.tablejs-sortsymbol').replaceWith('<span class="tablejs-sortsymbol">'+ symbol +'</span>');
+                    }
                     
                     for (i = 0; i < sortrows.length; i++)
                     {
@@ -161,14 +243,14 @@
                         {
                             if (mode == 'asc')
                             {
-                                if (findInRow(sortrows[j], sortby) < findInRow(sortrows[temp], sortby))
+                                if (findInRow(sortrows[j], sortby, attr.datecolumns) < findInRow(sortrows[temp], sortby, attr.datecolumns))
                                 {
                                     temp = j;
                                 }
                             }
                             else if (mode == 'desc')
                             {
-                                if (findInRow(sortrows[j], sortby) > findInRow(sortrows[temp], sortby))
+                                if (findInRow(sortrows[j], sortby, attr.datecolumns) > findInRow(sortrows[temp], sortby, attr.datecolumns))
                                 {
                                     temp = j;
                                 }
@@ -182,6 +264,11 @@
                     elm.find('tr:not(.tablejs-trh)').remove();
                     elm.append(sortrows);
                 });
+                
+                if (attr.startsorted != false)
+                {
+                    $('.tablejs-sort[rel="'+ attr.startsorted +'"]').click();
+                }
             });
         }
     }
